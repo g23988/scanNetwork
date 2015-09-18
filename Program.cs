b389817d853b7132cb5c5629f1fa68fc6af20392
g23988 <g23988@gmail.com>
2015-09-18 18:00:42 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+
+namespace scanNetwork
+{
+    class Program
+    {
+        private static config config = new config();
+        //回收的多執行續數量
+        private static int backcount = 0;
+
+        static void Main(string[] args)
+        {
+            //讀取輸入的資料
+            Readinput();
+            //多執行續啟動
+            ThreadPool.SetMaxThreads(config.maxThread,config.maxThread);
+
+            foreach (string item in config.getList())
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(checkPing), item);
+            }
+
+            while (true)
+            {
+                if (config.listlen() == backcount)
+                {
+                    Console.WriteLine("done!");
+                    Console.WriteLine(backcount.ToString());
+                    Console.ReadLine();
+                    break;
+                }
+            }
+        }
+
+        //多執行續處理的部分
+        private static void checkPing(object obj) {
+            string ip = (string)obj;
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+            options.DontFragment = true;
+            
+            //Console.WriteLine((string)obj);
+            backcount++;
+        }
+        
+
+
+
+
+
+
+
+        private static void Readinput(){
+            Console.WriteLine("輸入本地ipv4,預設是"+config.ip);
+            string inputip = Console.ReadLine();
+            if (inputip != "")
+            {
+                config.ip = inputip;
+            }
+            Console.WriteLine("輸入需要的範圍,class C or D,預設是"+config.range);
+            string inputrange = Console.ReadLine();
+            if (inputrange!="")
+            {
+                config.range = inputrange;    
+            }
+            Console.WriteLine("輸入允許執行thread的最大數量,預設是"+config.maxThread.ToString());
+            string inputmaxThread = Console.ReadLine();
+            if (inputmaxThread!="")
+            {
+                config.maxThread = int.Parse(inputmaxThread);
+            }
+            //創造清單
+            if (config.createList())
+            {
+                foreach (string item in config.getList())
+                {
+                    //Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("fail...");
+            }
+            
+
+            //Console.ReadLine();
+
+        }
+    }
+}
---
+++ config.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace scanNetwork
+{
+    class config
+    {
+        //起始計算的ip
+        private string _ip = "127.0.0.1";
+        //起始計算的mask
+        private string _range = "D";
+        //保存要檢查的ip列表
+        private List<string> _iplist = new List<string>();
+        //多執行續的數量
+        private int _maxThread = 10;
+        
+
+        /// <summary>
+        /// 起始點的ip,預設 127.0.0.1
+        /// </summary>
+        public string ip {
+            get { return _ip; }
+            set { _ip = value; }
+        }
+        /// <summary>
+        /// 設定範圍 C or D,預設 D
+        /// </summary>
+        public string range {
+            get { return _range; }
+            set { _range = value; }
+        }
+        /// <summary>
+        /// 執行續的最大數量,預設 10
+        /// </summary>
+        public int maxThread {
+            get { return _maxThread; }
+            set { _maxThread = value;}
+        }
+
+
+
+        /// <summary>
+        /// 取得待檢查的ip列表
+        /// </summary>
+        public Boolean createList() {
+            try
+            {
+                bool result = false;
+                string range = this._range;
+                switch (range.ToLower())
+                {
+                    case "c":
+                        for (int i = 1; i < 254; i++)
+                        {
+                            for (int j = 1; j < 254; j++)
+                            {
+string[] cut = Regex.Split(_ip,@"\.");
+                                string thisIP = cut[0] + "." + cut[1] + "." + i + "." + j;
+                                _iplist.Add(thisIP);
+                            }
+                        }
+                        result = true;
+                        break;
+
+                    case "d":
+                        for (int i = 1; i < 254; i++)
+                        {
+                                string[] cut = Regex.Split(_ip,@"\.");
+                                string thisIP = cut[0] + "." + cut[1] + "." + cut[2] + "." + i;
+                                _iplist.Add(thisIP);
+                        }
+                        result = true;
+                        break;
+                    default:
+                        result = false;
+                        break;
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        /// <summary>
+        /// 取得所有ip列表
+        /// </summary>
+        /// <returns></returns>
+        public List<string> getList() {
+            return _iplist;
+        }
+        /// <summary>
+        /// 取得ip列表的數量
+        /// </summary>
+        /// <returns></returns>
+        public int listlen() {
+            return _iplist.Count();
+        }
+        
+    }
+}
